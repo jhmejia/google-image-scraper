7c3562071f6ab0dde320d55c73d59e3ebbed1599
John Henry Mejia <johnhmejia02@gmail.com>
2023-02-24 12:44:20 -0600

    add quilt

--- imagescraper.py
+++ imagescraper.py
@@ -136,6 +136,11 @@ def download_image(folder_path: str, url: str, i: int, index: int, quantity: int
         r = requests.get(url, stream=True)
         if r.status_code == 200:
             image_number = i + quantity * index
+            # #Make sure file path exists. If not, create it.
+            # if not os.path.exists(folder_path):
+            #     os.makedirs(folder_path)
+
+            # Save image to file.
             with open(folder_path + str(image_number) + '.jpg', 'wb') as f:
                 r.raw.decode_content = True
                 shutil.copyfileobj(r.raw, f)
